18a7ab2402a0a5ca5a58e5e82241b09511125917
MayraMendoza <mayra601m@gmail.com>
2022-09-10 19:29:59 -0400

    complete

--- src/com/HackerRankInterviewPrep/week1/PlusMinus.java
+++ src/com/HackerRankInterviewPrep/week1/PlusMinus.java
@@ -15,12 +15,29 @@ import static java.util.stream.Collectors.toList;
 
 class PlusMinus {
 
-    /*Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
-
-Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to (10^-4) are acceptable.
+    /**Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+     *Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to (10^-4) are acceptable.
      * Complete the 'plusMinus' function below.
      *
+
      * The function accepts INTEGER_ARRAY arr as parameter.
+     Sample Input
+
+
+
+     */
+
+    /**
+     * STDIN           Function
+     * -----           --------
+     * 6               arr[] size n = 6
+     * -4 3 -9 0 4 1   arr = [-4, 3, -9, 0, 4, 1]
+     * Sample Output
+     *
+     * 0.500000
+     * 0.333333
+     * 0.166667
+     *
      */
 
     public static void plusMinus1(List<Integer> arr) {
