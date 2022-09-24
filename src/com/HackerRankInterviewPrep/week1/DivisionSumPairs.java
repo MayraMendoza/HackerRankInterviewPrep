e46370814183e00fc9c3816b23b95ffbbcb1a388
MayraMendoza <mayra601m@gmail.com>
2022-09-23 22:35:27 -0400

    Division sum pairs

--- src/com/HackerRankInterviewPrep/week1/DivisionSumPairs.java
+++ src/com/HackerRankInterviewPrep/week1/DivisionSumPairs.java
@@ -21,6 +21,7 @@ class DivisionSumPairs {
      * Given an array of integers and a positive integer k, determine the number of (i,j)  pairs where i<j  and ar[i] + ar[j]  is divisible by k.
      * The function is expected to return an INTEGER.
      * The function accepts following parameters:
+     * make sure  i< j.
      *  1. INTEGER n
      *  2. INTEGER k
      *  3. INTEGER_ARRAY ar
