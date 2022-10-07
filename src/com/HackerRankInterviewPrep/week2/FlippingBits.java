0e212acdd58265ce960d1a0af462cd3fd664317d
MayraMendoza <mayra601m@gmail.com>
2022-10-06 21:16:19 -0400

    Flipping bits

---
+++ src/com/HackerRankInterviewPrep/week2/FlippingBits.java
@@ -0,0 +1,19 @@
+package com.HackerRankInterviewPrep.week2;
+
+
+//You will be given a list of 32 bit unsigned integers. Flip all the bits ( and ) and return the result as an unsigned integer.
+//
+//Example 0 ->1  or 1->0
+public class FlippingBits {
+//    public static long flippingBits(long n) {
+//        // Write your code here
+//        // convert to binary = flip then find lenght and pad.
+//
+//
+//    }
+
+
+    public static void main(String[] args) {
+    }
+
+}
