9b44041bef982c648090742b90117a1f2b4fa1d9
MayraMendoza <mayra601m@gmail.com>
2022-10-11 20:24:34 -0400

    SubarrayDivision2 complete

--- src/com/HackerRankInterviewPrep/Week3/SubarrayDivision2.java
+++ src/com/HackerRankInterviewPrep/Week3/SubarrayDivision2.java
@@ -9,6 +9,32 @@ package com.HackerRankInterviewPrep.Week3;
 //        Determine how many ways she can divide the chocolate.
 
 
+import java.util.List;
 
 public class SubarrayDivision2 {
+    public static int birthday(List<Integer> s, int d, int m) {
+        // Write your code here
+        // System.out.println(d +"  " + m);
+        // System.out.print(s.size());
+        int DiffWaysToDivideBar= 0 ;
+        int sum = 0;
+        for( int i = 0; i< s.size()-m+1; i++){
+            sum+= s.get(i);
+            // System.out.println("i" +i);
+            for(int j = i+1; j< i+m ; j++){
+                sum += s.get(j);
+                // System.out.print(" " + j + "");
+            }
+            if(sum == d){
+                DiffWaysToDivideBar++;
+                sum =0;
+            } else{
+                sum=0;
+            }
+        }
+        return DiffWaysToDivideBar;
+
+    }
+
 }
+
