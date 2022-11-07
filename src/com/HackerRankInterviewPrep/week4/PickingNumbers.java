5a438345249d1828bf6bf1459fa32d5fb5c9f96c
MayraMendoza <mayra601m@gmail.com>
2022-11-07 18:33:18 -0500

    picking numbers- complete

--- src/com/HackerRankInterviewPrep/week4/PickingNumbers.java
+++ src/com/HackerRankInterviewPrep/week4/PickingNumbers.java
@@ -12,37 +12,30 @@ public class PickingNumbers {
         // Write your code here
         // this will sort array
         Collections.sort(a);
-        System.out.println(a);
         // now go through the array
         int countLongestSubarray = 0;
         int count =0;
 
 
-        for(int i = 0; i < a.size()-1;i++){
+        for(int i = 0; i < a.size();i++){
             count =0;
-            for(int j = 0+i; j < a.size()-i; j++ ){
+            if(i>1 && Math.abs(a.get(i-1)+ a.get(i))<=1){
+                count = count+1;
+            }
+            for(int j = 0+i; j < a.size(); j++ ){
+                System.out.print("this");
                 System.out.println("i"+a.get(i));
                 System.out.println("j" + a.get(j));
-                if(Math.abs(a.get(i)- a.get(j))<=1){
+                if(Math.abs(a.get(i)- a.get(j))<=1) {
                     count++;
                     System.out.println(count);
-
-                } else {
-
-                    System.out.print("break");
-                    break;
                 }
-
-
             }
-            if(countLongestSubarray<count && count<= a.size()){
+            if(countLongestSubarray<count && count<= a.size()) {
                 countLongestSubarray = count;
-                count=0;
+                count = 0;
             }
         }
-
-
-
         return countLongestSubarray;
     }
 }
