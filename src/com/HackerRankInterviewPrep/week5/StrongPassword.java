bbf5bf9cb2b922d27a54763ae6b02d699fec8029
MayraMendoza <mayra601m@gmail.com>
2022-11-10 22:23:43 -0500

    strong password progress

---
+++ src/com/HackerRankInterviewPrep/week5/StrongPassword.java
@@ -0,0 +1,28 @@
+package com.HackerRankInterviewPrep.week5;
+
+/**
+ * Louise joined a social networking site to stay in touch with her friends. The signup page required her to input a name and a password. However, the password must be strong. The website considers a password to be strong if it satisfies the following criteria:
+ *
+ * Its length is at least 6.
+ * It contains at least one digit.
+ * It contains at least one lowercase English character.
+ * It contains at least one uppercase English character.
+ * It contains at least one special character. The special characters are: !@#$%^&*()-+
+ * She typed a random string of length n in the password field but wasn't sure if it was strong. Given the string she typed, can you find the minimum number of characters she must add to make her password strong?
+ *
+ * Note: Here's the set of types of characters in a form you can paste in your solution:
+ */
+public class StrongPassword {
+    public static int minimumNumber(int n, String password) {
+        // Return the minimum number of characters to make the password strong
+        //  [a-z], [A-Z], [0-9], or [!@#$%^&*()-+ ].
+
+        // make for loop to check if these are present
+        // if they are present then add to counter.
+
+
+    }
+
+
+
+}
